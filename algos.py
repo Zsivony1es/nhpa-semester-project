c09e568b7d979996a6b72646a453bda1c258494b
Peter Ivony <peterivony@MacBook-Air-von-Peter.local>
2024-01-14 16:40:31 +0100

    Initial commit

---
+++ algos.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class Algorithms:
+    def __init__(self):
+        pass
+
+    def select(column: np.ndarray) -> (int, float):
+
+        value = 0.0
+        index = 0
+
+        d = 0
+        length = column.shape[0]
+        for i in range(length):
+            d += column[i]
+            prob = column[i] / d
+            if numpy.random.choice([True, False], p=[prob, 1-prob]):
+                index = i
+                value = column[i]
+        
+        return (index, value)
+
+    def opt_select(column: np.ndarray) -> (int, float):
+        cumulative_sum = np.cumsum(column)
+        total_sum = cumulative_sum[-1]
+
+        random_value = np.random.uniform(0, total_sum)
+        index = np.searchsorted(cumulative_sum, random_value)
+
+        return index, column[index]
+
+
+
