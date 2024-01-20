f2a3559aaaa370ad32815868681e9637d59da725
Peter Ivony <peterivony@protonmail.com>
2024-01-20 20:51:03 +0100

    Correct BMM algorithm

--- src/algos.py
+++ src/algos.py
@@ -34,18 +34,27 @@ class Algorithms:
 
         return index, column[index]
 
-    def basic_matrix_mult(A: np.ndarray, B: np.ndarray, c: int, prob: np.ndarray) -> np.ndarray:
+    def basic_matrix_mult(A: np.ndarray, B: np.ndarray, c: int) -> np.ndarray:
         assert A.shape[1] == B.shape[0], f"The dimensions of A ({A.shape}) and B ({B.shape}) don't match!"
         assert 1 <= c <= A.shape[1], f"The c value must be between 1 and {A.shape[1]}!"
-        assert prob.shape[0] == A.shape[1], \
-            f"'prob' must contain {A.shape[1]} entries, but it contains {prob.shape[0]}!"
-        assert np.isclose(prob.sum(), 1), f"The sum of the probabilities must be 1, but it is {prob.sum()}!"
 
+        # Create the probability distribution
+        total_AiBi = 0
+        prob = np.empty(A.shape[1])
+        for i in range(A.shape[1]):
+            prob[i] = np.linalg.norm(Algorithms.select(A[:, i])) * np.linalg.norm(Algorithms.select(B[i, :]))
+            total_AiBi += prob[i]
+        prob /= total_AiBi
+
+        assert np.isclose(prob.sum(), 1), "The probabilities should add up to 1!"
+
+        # Initialize C and R
         C = np.empty((A.shape[0], c))
         R = np.empty((c, B.shape[1]))
 
+        # Calculate C and R
         for t in range(c):
-            i_t = np.random.choice(np.arange(A.shape[1]), prob, replace=True)
+            i_t = np.random.choice(np.arange(A.shape[1]), p=prob, replace=True)
             C[:, t] = A[:, i_t] / math.sqrt(c * prob[i_t])
             R[t, :] = B[i_t, :] / math.sqrt(c * prob[i_t])
 
